ade12cb73d6384af030139c9442330b6462ed1bd
Andrey Kozhevnikov <freazeek@mail.ru>
2015-09-18 23:09:53 +0300

    test colliz

--- Budget/ViewModels/EnterViewModel.cs
+++ Budget/ViewModels/EnterViewModel.cs
@@ -15,7 +15,7 @@ namespace Budget {
             MakeLists();//+
             UpdateTags(); //+
 
-            CreateNewCurrentOrder();//
+            CreateNewCurrentOrder();//te
             SetFocusOnTextEditValue();
 
 
