272534247f279f0baf353f14cea3311025c84cad
Andrey K (home) <freazeek@mail.ru>
2016-01-31 22:27:45 +0300

    Fix - build events

--- Budget/Properties/AssemblyInfo.cs
+++ Budget/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("16.1.27.2237")]
-[assembly: AssemblyVersion("16.1.27.2237")]
-[assembly: AssemblyFileVersion("16.1.27.2237")]
+// [assembly: AssemblyVersion("16.1.31.2225")]
+[assembly: AssemblyVersion("16.1.31.2225")]
+[assembly: AssemblyFileVersion("16.1.31.2225")]
