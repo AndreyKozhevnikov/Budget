937776da09dcc27f2d0a8a2f5829178856ed1120
Andrey K (home) <freazeek@mail.ru>
2016-03-01 21:57:28 +0300

    PieChart - default values

--- Budget/Properties/AssemblyInfo.cs
+++ Budget/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("16.2.20.0819")]
-[assembly: AssemblyVersion("16.2.20.0819")]
-[assembly: AssemblyFileVersion("16.2.20.0819")]
+// [assembly: AssemblyVersion("16.3.1.2157")]
+[assembly: AssemblyVersion("16.3.1.2157")]
+[assembly: AssemblyFileVersion("16.3.1.2157")]
