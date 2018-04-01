5466fa52a8807f81c5ee63e9abea3e92ddfa203e
Andrey <freazeek@mail.ru>
2018-04-01 15:02:06 +0300

    fix place unused tags at the bottom

--- Budget/Classes/MyIntInListPositionComparer.cs
+++ Budget/Classes/MyIntInListPositionComparer.cs
@@ -6,20 +6,24 @@ using System.Threading.Tasks;
 
 namespace Budget {
 
-        class MyIntInListPositionComparer<T> : IComparer<int> {
-            public MyIntInListPositionComparer(List<int> _innerList) {
-                innerList = _innerList;
-            }
-
-            List<int> innerList;
-            public int Compare(int x, int y) {
-                var index1 = innerList.IndexOf(x);
-                var index2 = innerList.IndexOf(y);
-                var res = Comparer<int>.Default.Compare(index1, index2);
-
-                //   Debug.Print(string.Format("{0} {1}  - {2} ", index1, index2, res));
-                return res;
-            }
+    class MyIntInListPositionComparer<T> : IComparer<int> {
+        public MyIntInListPositionComparer(List<int> _innerList) {
+            innerList = _innerList;
         }
-    
+
+        List<int> innerList;
+        public int Compare(int x, int y) {
+            var index1 = innerList.IndexOf(x);
+            var index2 = innerList.IndexOf(y);
+            var res = Comparer<int>.Default.Compare(index1, index2);
+            if (res != 0) {
+                if (index1 < 0)
+                    res = 1;
+                if (index2 < 0)
+                    res = -1;
+            }
+            return res;
+        }
+    }
+
 }
