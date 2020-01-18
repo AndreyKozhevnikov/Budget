c3e0f9cee9f3e34795c75ada7d9c7b065790e70e
Andrey <freazeek@mail.ru>
2020-01-18 19:05:20 +0300

    fix wrong filter

--- Budget/Properties/AssemblyInfo.cs
+++ Budget/Properties/AssemblyInfo.cs
@@ -42,6 +42,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("18.12.18.1815")]
-[assembly: AssemblyVersion("18.12.18.1815")]
-[assembly: AssemblyFileVersion("18.12.18.1815")]
+// [assembly: AssemblyVersion("20.1.18.1903")]
+[assembly: AssemblyVersion("20.1.18.1903")]
+[assembly: AssemblyFileVersion("20.1.18.1903")]
